e132cacb1b460f36538aad7667c67b4003a6032c
f3n1xx <arkspeed@gmail.com>
2021-06-18 01:09:24 +0200

    LinkedList

--- Day 1-100/day1.py
+++ Day 1-100/day1.py
@@ -17,8 +17,14 @@ class Node:
 
 # Class that represents the LL. Start of Linked list is head and that's all we need.
 class LinkedList:
-    def __init__(self):
+    def __init__(self, nodes=None):
         self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
 
     def __repr__(self):
         node = self.head  # First element
@@ -28,19 +34,30 @@ class LinkedList:
         while node is not None:
             nodes.append(node.data)
             node = node.next
+        self.llist = nodes
         nodes.append("Empty")
         return " -> ".join(nodes)
 
     def rev(self):
-        print()
+        head = self.head
+        self.__repr__()
+        linkedlist = self.llist
+        linkedlist.pop(-1)
+        linkedlist.reverse()
+        # TODO: String representation is reversed, but data structre is not, need to reassign data and next again.
+        linkedlist.append("Empty")
+        return " -> ".join(linkedlist)
+
+
 
 
 if __name__ == '__main__':
-    llist = LinkedList()
-    first_node = Node("a")
-    llist.head = first_node
-    second_node = Node("b")
-    third_node = Node("c")
-    first_node.next = second_node
-    second_node.next = third_node
-    print(llist)
+    llist = LinkedList(['a', 'b', 'c', 'd'])
+    # first_node = Node("a")
+    # llist.head = first_node
+    # second_node = Node("b")
+    # third_node = Node("c")
+    # first_node.next = second_node
+    # second_node.next = third_node
+    # print(llist)
+    print(llist.rev())
